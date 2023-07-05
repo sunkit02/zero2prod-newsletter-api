626dac7d778978fa26d2e21f6fb245c1fc6b3daf
Sun Kit Tsui <sunkittsui@gmail.com>
2023-07-04 21:54:13 -0500

    Finish chapter 4

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,3 +1,6 @@
+use secrecy::Secret;
+use secrecy::ExposeSecret;
+
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
@@ -7,7 +10,7 @@ pub struct Settings {
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
-    pub password: String,
+    pub password: Secret<String>,
     pub port: u16,
     pub host: String,
     pub database_name: String,
@@ -17,14 +20,14 @@ impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password, self.host, self.port, self.database_name
+            self.username, self.password.expose_secret(), self.host, self.port, self.database_name
         )
     }
 
     pub fn connection_string_without_db(&self) -> String {
         format!(
             "postgres://{}:{}@{}:{}",
-            self.username, self.password, self.host, self.port
+            self.username, self.password.expose_secret(), self.host, self.port
         )
     }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -57,9 +57,10 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let mut connection = 
+        PgConnection::connect(&config.connection_string_without_db())
+            .await
+            .expect("Failed to connect to Postgres.");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
